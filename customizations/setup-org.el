6391bfcc36b55b23ee6ed21c333ccca04c97a52c
Lev Lazinskiy <lev@levlaz.org>
2016-07-09 21:33:42 -0700

    Update org

--- customizations/setup-org.el
+++ customizations/setup-org.el
@@ -7,3 +7,23 @@
 
 ;; Load ODT backend to allow for exporting to open document format.
 (require 'ox-odt)
+
+;;;;
+;; Projects
+;;;;
+
+;; levlaz.org Blog
+(setq org-publish-project-alist
+      '(("levlaz"
+         ;; Path to org files.
+         :base-directory "~/git/levlaz.org/_org"
+         :base-extension "org"
+
+         ;; Path to Jekyll Posts
+         :publishing-directory "~/git/levlaz.org/_posts/2016/"
+         :recursive t
+         :publishing-function org-html-publish-to-html
+         :headline-levels 4
+         :html-extension "html"
+         :body-only t
+         )))
