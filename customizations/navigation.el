0a99279d68f22988eb12de269d9c99d53b32604d
Lev Lazinskiy <lev@levlaz.org>
2016-07-11 12:19:51 -0700

    ido-mode customizations
    Signed-off-by: Lev Lazinskiy <lev@levlaz.org>

--- customizations/navigation.el
+++ customizations/navigation.el
@@ -2,3 +2,13 @@
 
 ;; Use Projectile everywhere
 (projectile-global-mode)
+
+;;;;
+;; ido customizations
+;;;;
+(ido-mode t)
+(setq ido-enabe-flex-matching t)
+(setq ido-use-filename-at-point nil)
+(setq ido-auto-merge-work-directories-length -1)
+(setq ido-use-virtual-buffers t)
+(ido-ubiquitous-mode 1)
